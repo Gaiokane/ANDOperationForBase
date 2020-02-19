484240661f97f18e5d7c041cdaeea7221f96e9c8
Gaiokane <qk632773809@vip.qq.com>
2020-02-19 22:04:46 +0800

    等比数列 与运算 转换 完成

--- ANDOperationForBase/Form1.Designer.cs
+++ ANDOperationForBase/Form1.Designer.cs
@@ -28,13 +28,191 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.chkBox1 = new System.Windows.Forms.CheckBox();
+            this.chkBox2 = new System.Windows.Forms.CheckBox();
+            this.chkBox4 = new System.Windows.Forms.CheckBox();
+            this.chkBox8 = new System.Windows.Forms.CheckBox();
+            this.chkBox16 = new System.Windows.Forms.CheckBox();
+            this.chkBox32 = new System.Windows.Forms.CheckBox();
+            this.chkBox64 = new System.Windows.Forms.CheckBox();
+            this.chkBox128 = new System.Windows.Forms.CheckBox();
+            this.chkBox256 = new System.Windows.Forms.CheckBox();
+            this.chkBox512 = new System.Windows.Forms.CheckBox();
+            this.txtBoxValue = new System.Windows.Forms.TextBox();
+            this.btnNumToChkBox = new System.Windows.Forms.Button();
+            this.btnChkBoxToNum = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // chkBox1
+            // 
+            this.chkBox1.AutoSize = true;
+            this.chkBox1.Location = new System.Drawing.Point(12, 17);
+            this.chkBox1.Name = "chkBox1";
+            this.chkBox1.Size = new System.Drawing.Size(78, 16);
+            this.chkBox1.TabIndex = 1;
+            this.chkBox1.Text = "checkBox1";
+            this.chkBox1.UseVisualStyleBackColor = true;
+            // 
+            // chkBox2
+            // 
+            this.chkBox2.AutoSize = true;
+            this.chkBox2.Location = new System.Drawing.Point(96, 17);
+            this.chkBox2.Name = "chkBox2";
+            this.chkBox2.Size = new System.Drawing.Size(78, 16);
+            this.chkBox2.TabIndex = 2;
+            this.chkBox2.Text = "checkBox2";
+            this.chkBox2.UseVisualStyleBackColor = true;
+            // 
+            // chkBox4
+            // 
+            this.chkBox4.AutoSize = true;
+            this.chkBox4.Location = new System.Drawing.Point(180, 17);
+            this.chkBox4.Name = "chkBox4";
+            this.chkBox4.Size = new System.Drawing.Size(78, 16);
+            this.chkBox4.TabIndex = 3;
+            this.chkBox4.Text = "checkBox3";
+            this.chkBox4.UseVisualStyleBackColor = true;
+            // 
+            // chkBox8
+            // 
+            this.chkBox8.AutoSize = true;
+            this.chkBox8.Location = new System.Drawing.Point(264, 17);
+            this.chkBox8.Name = "chkBox8";
+            this.chkBox8.Size = new System.Drawing.Size(78, 16);
+            this.chkBox8.TabIndex = 4;
+            this.chkBox8.Text = "checkBox4";
+            this.chkBox8.UseVisualStyleBackColor = true;
+            // 
+            // chkBox16
+            // 
+            this.chkBox16.AutoSize = true;
+            this.chkBox16.Location = new System.Drawing.Point(348, 17);
+            this.chkBox16.Name = "chkBox16";
+            this.chkBox16.Size = new System.Drawing.Size(78, 16);
+            this.chkBox16.TabIndex = 5;
+            this.chkBox16.Text = "checkBox5";
+            this.chkBox16.UseVisualStyleBackColor = true;
+            // 
+            // chkBox32
+            // 
+            this.chkBox32.AutoSize = true;
+            this.chkBox32.Location = new System.Drawing.Point(12, 52);
+            this.chkBox32.Name = "chkBox32";
+            this.chkBox32.Size = new System.Drawing.Size(78, 16);
+            this.chkBox32.TabIndex = 6;
+            this.chkBox32.Text = "checkBox6";
+            this.chkBox32.UseVisualStyleBackColor = true;
+            // 
+            // chkBox64
+            // 
+            this.chkBox64.AutoSize = true;
+            this.chkBox64.Location = new System.Drawing.Point(96, 52);
+            this.chkBox64.Name = "chkBox64";
+            this.chkBox64.Size = new System.Drawing.Size(78, 16);
+            this.chkBox64.TabIndex = 7;
+            this.chkBox64.Text = "checkBox7";
+            this.chkBox64.UseVisualStyleBackColor = true;
+            // 
+            // chkBox128
+            // 
+            this.chkBox128.AutoSize = true;
+            this.chkBox128.Location = new System.Drawing.Point(180, 52);
+            this.chkBox128.Name = "chkBox128";
+            this.chkBox128.Size = new System.Drawing.Size(78, 16);
+            this.chkBox128.TabIndex = 8;
+            this.chkBox128.Text = "checkBox8";
+            this.chkBox128.UseVisualStyleBackColor = true;
+            // 
+            // chkBox256
+            // 
+            this.chkBox256.AutoSize = true;
+            this.chkBox256.Location = new System.Drawing.Point(264, 52);
+            this.chkBox256.Name = "chkBox256";
+            this.chkBox256.Size = new System.Drawing.Size(78, 16);
+            this.chkBox256.TabIndex = 9;
+            this.chkBox256.Text = "checkBox9";
+            this.chkBox256.UseVisualStyleBackColor = true;
+            // 
+            // chkBox512
+            // 
+            this.chkBox512.AutoSize = true;
+            this.chkBox512.Location = new System.Drawing.Point(348, 52);
+            this.chkBox512.Name = "chkBox512";
+            this.chkBox512.Size = new System.Drawing.Size(84, 16);
+            this.chkBox512.TabIndex = 10;
+            this.chkBox512.Text = "checkBox10";
+            this.chkBox512.UseVisualStyleBackColor = true;
+            // 
+            // txtBoxValue
+            // 
+            this.txtBoxValue.Location = new System.Drawing.Point(438, 12);
+            this.txtBoxValue.Name = "txtBoxValue";
+            this.txtBoxValue.Size = new System.Drawing.Size(164, 21);
+            this.txtBoxValue.TabIndex = 11;
+            // 
+            // btnNumToChkBox
+            // 
+            this.btnNumToChkBox.Location = new System.Drawing.Point(438, 45);
+            this.btnNumToChkBox.Name = "btnNumToChkBox";
+            this.btnNumToChkBox.Size = new System.Drawing.Size(79, 23);
+            this.btnNumToChkBox.TabIndex = 12;
+            this.btnNumToChkBox.Text = "num->chkbox";
+            this.btnNumToChkBox.UseVisualStyleBackColor = true;
+            this.btnNumToChkBox.Click += new System.EventHandler(this.btnNumToChkBox_Click);
+            // 
+            // btnChkBoxToNum
+            // 
+            this.btnChkBoxToNum.Location = new System.Drawing.Point(523, 45);
+            this.btnChkBoxToNum.Name = "btnChkBoxToNum";
+            this.btnChkBoxToNum.Size = new System.Drawing.Size(79, 23);
+            this.btnChkBoxToNum.TabIndex = 13;
+            this.btnChkBoxToNum.Text = "chkbox->num";
+            this.btnChkBoxToNum.UseVisualStyleBackColor = true;
+            this.btnChkBoxToNum.Click += new System.EventHandler(this.btnChkBoxToNum_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(622, 85);
+            this.Controls.Add(this.btnChkBoxToNum);
+            this.Controls.Add(this.btnNumToChkBox);
+            this.Controls.Add(this.txtBoxValue);
+            this.Controls.Add(this.chkBox512);
+            this.Controls.Add(this.chkBox256);
+            this.Controls.Add(this.chkBox128);
+            this.Controls.Add(this.chkBox64);
+            this.Controls.Add(this.chkBox32);
+            this.Controls.Add(this.chkBox16);
+            this.Controls.Add(this.chkBox8);
+            this.Controls.Add(this.chkBox4);
+            this.Controls.Add(this.chkBox2);
+            this.Controls.Add(this.chkBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "等比数列与运算";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.CheckBox chkBox1;
+        private System.Windows.Forms.CheckBox chkBox2;
+        private System.Windows.Forms.CheckBox chkBox4;
+        private System.Windows.Forms.CheckBox chkBox8;
+        private System.Windows.Forms.CheckBox chkBox16;
+        private System.Windows.Forms.CheckBox chkBox32;
+        private System.Windows.Forms.CheckBox chkBox64;
+        private System.Windows.Forms.CheckBox chkBox128;
+        private System.Windows.Forms.CheckBox chkBox256;
+        private System.Windows.Forms.CheckBox chkBox512;
+        private System.Windows.Forms.TextBox txtBoxValue;
+        private System.Windows.Forms.Button btnNumToChkBox;
+        private System.Windows.Forms.Button btnChkBoxToNum;
     }
 }
 
--- ANDOperationForBase/Form1.cs
+++ ANDOperationForBase/Form1.cs
@@ -16,5 +16,194 @@ namespace ANDOperationForBase
         {
             InitializeComponent();
         }
+
+        #region 窗体加载事件
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //label1.Text = (128 & 256).ToString();
+            chkBox1.Text = "污染源";
+            chkBox2.Text = "客户";
+            chkBox4.Text = "供应商";
+            chkBox8.Text = "考核单位";
+            chkBox16.Text = "分包商";
+            chkBox32.Text = "生产厂商";
+            chkBox64.Text = "鉴定机构";
+            chkBox128.Text = "运营商";
+            chkBox256.Text = "256";
+            chkBox512.Text = "512";
+
+            txtBoxValue.Focus();
+        }
+        #endregion
+
+        #region 复选框转数字 按钮单击事件
+        private void btnNumToChkBox_Click(object sender, EventArgs e)
+        {
+            string str = txtBoxValue.Text.Trim();
+            if (IsNumber(str) == false || string.IsNullOrEmpty(str))
+            {
+                MessageBox.Show("只能输入数字！");
+                txtBoxValue.Focus();
+            }
+            else
+            {
+                resetChkBoxChecked();
+                int value = Convert.ToInt32(txtBoxValue.Text.Trim());
+                if (getAndOperationResult(value, 1) != 0)
+                {
+                    chkBox1.Checked = true;
+                }
+                if (getAndOperationResult(value, 2) != 0)
+                {
+                    chkBox2.Checked = true;
+                }
+                if (getAndOperationResult(value, 4) != 0)
+                {
+                    chkBox4.Checked = true;
+                }
+                if (getAndOperationResult(value, 8) != 0)
+                {
+                    chkBox8.Checked = true;
+                }
+                if (getAndOperationResult(value, 16) != 0)
+                {
+                    chkBox16.Checked = true;
+                }
+                if (getAndOperationResult(value, 32) != 0)
+                {
+                    chkBox32.Checked = true;
+                }
+                if (getAndOperationResult(value, 64) != 0)
+                {
+                    chkBox64.Checked = true;
+                }
+                if (getAndOperationResult(value, 128) != 0)
+                {
+                    chkBox128.Checked = true;
+                }
+                if (getAndOperationResult(value, 256) != 0)
+                {
+                    chkBox256.Checked = true;
+                }
+                if (getAndOperationResult(value, 512) != 0)
+                {
+                    chkBox512.Checked = true;
+                }
+            }
+        }
+        #endregion
+
+        #region 数字转复选框 按钮单击事件
+        private void btnChkBoxToNum_Click(object sender, EventArgs e)
+        {
+            int result = 0;
+            int num = 0;
+            if (chkBox1.Checked == true)
+            {
+                num = 1;
+                result += num;
+            }
+            if (chkBox2.Checked == true)
+            {
+                num = 2;
+                result += num;
+            }
+            if (chkBox4.Checked == true)
+            {
+                num = 4;
+                result += num;
+            }
+            if (chkBox8.Checked == true)
+            {
+                num = 8;
+                result += num;
+            }
+            if (chkBox16.Checked == true)
+            {
+                num = 16;
+                result += num;
+            }
+            if (chkBox32.Checked == true)
+            {
+                num = 32;
+                result += num;
+            }
+            if (chkBox64.Checked == true)
+            {
+                num = 64;
+                result += num;
+            }
+            if (chkBox128.Checked == true)
+            {
+                num = 128;
+                result += num;
+            }
+            if (chkBox256.Checked == true)
+            {
+                num = 256;
+                result += num;
+            }
+            if (chkBox512.Checked == true)
+            {
+                num = 512;
+                result += num;
+            }
+            txtBoxValue.Text = result.ToString();
+        }
+        #endregion
+
+        #region 将所有chkbox的checked属性置为false
+        /// <summary>
+        /// 将所有chkbox的checked属性置为false
+        /// </summary>
+        private void resetChkBoxChecked()
+        {
+            chkBox1.Checked = false;
+            chkBox2.Checked = false;
+            chkBox4.Checked = false;
+            chkBox8.Checked = false;
+            chkBox16.Checked = false;
+            chkBox32.Checked = false;
+            chkBox64.Checked = false;
+            chkBox128.Checked = false;
+            chkBox256.Checked = false;
+            chkBox512.Checked = false;
+        }
+        #endregion
+
+        #region 与运算，返回结果，0 or 非0
+        /// <summary>
+        /// 与运算，返回结果，0 or 非0
+        /// </summary>
+        /// <param name="value">value & num</param>
+        /// <param name="num">value & num</param>
+        /// <returns></returns>
+        private int getAndOperationResult(int value, int num)
+        {
+            int result = value & num;
+            return result;
+        }
+        #endregion
+
+        #region 判断字符串是否为数字
+        /// <summary>
+        /// 判断字符串是否为数字
+        /// </summary>
+        /// <param name="str">待验证的字符串</param>
+        /// <returns>bool</returns>
+        public static bool IsNumber(string str)
+        {
+            bool result = true;
+            foreach (char ar in str)
+            {
+                if (!char.IsNumber(ar))
+                {
+                    result = false;
+                    break;
+                }
+            }
+            return result;
+        }
+        #endregion
     }
 }
